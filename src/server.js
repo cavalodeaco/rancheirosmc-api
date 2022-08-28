dac92a831432a2007c6cf4749cda260e28de2cc7
Gian Maurício Fritsche <gianmauriciofritsche@MacBook-Air-de-Gian.local>
2022-08-28 16:18:07 -0300

    add cors

--- src/server.js
+++ src/server.js
@@ -2,8 +2,12 @@ require('dotenv').config()
 
 const express = require('express')
 const serverless = require('serverless-http')
+const cors = require('cors')
 
 const app = express()
+
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
