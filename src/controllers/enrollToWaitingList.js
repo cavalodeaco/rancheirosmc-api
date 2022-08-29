7b0a6e682c84ca223ca57dd891edd7f59f3d02d0
Gian Maurício Fritsche <gianmauriciofritsche@MacBook-Air-de-Gian.local>
2022-08-28 21:36:02 -0300

    change user id to string

--- src/controllers/enrollToWaitingList.js
+++ src/controllers/enrollToWaitingList.js
@@ -3,16 +3,16 @@ const { v4: uuidv4 } = require('uuid');
 
 module.exports = async (req, res) => {
   const { name } = req.body;
-  const User = dynamoose.model("NewUser", { "id": Number, "name": String });
+  const User = dynamoose.model("User", { "id": String, "name": String });
   const myUser = new User({
-    "id": 1,
+    "id": "1234",
     "name": name,
   });
 
   try {
     await myUser.save();
     console.log("Save operation was successful.");
-    const myOtherUser = await User.get(1);
+    const myOtherUser = await User.get("1234");
     console.log(myOtherUser);
     res.json({ myUser, myOtherUser })
   } catch (error) {
