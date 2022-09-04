f87302d0e7501783284a0cc8684b93ec4861636b
Giovanna G B Fritsche <ggarciabas@gmail.com>
2022-09-04 19:03:35 -0300

    fix: remove enroll

--- node-js/index.js
+++ node-js/index.js
@@ -2,12 +2,11 @@ import app from "./src/api/app.js";
 import dynamoose from "dynamoose";
 import dotenv from 'dotenv';
 import {UserModelDynamo} from './src/model/user-model.js';
-import {EnrollModelDynamo} from './src/model/enroll-model.js';
 
 dotenv.config()
 const { aws, Table } = dynamoose;
 aws.ddb.local();
 
-const TableDynamo = new Table(process.env.TABLE_NAME, [UserModelDynamo, EnrollModelDynamo]);
+const TableDynamo = new Table(process.env.TABLE_NAME, [UserModelDynamo]);
 
 app.listen(3000, () => console.log('I hear you, on http://localhost:3000'))
