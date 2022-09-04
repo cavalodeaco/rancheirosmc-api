5b9ac80159ae4800caed7a4dab5655bd30bb1a46
Giovanna G B Fritsche <ggarciabas@gmail.com>
2022-09-04 19:04:04 -0300

    fix: remove enroll

--- node-js/src/serverless.js
+++ node-js/src/serverless.js
@@ -1,8 +1,7 @@
 import app from "./api/app.js";
 import serverless from 'serverless-http';
 import {UserModelDynamo} from './src/model/user-model.js';
-import {EnrollModelDynamo} from './src/model/enroll-model.js';
 
-const TableDynamo = new Table(process.env.TABLE_NAME, [UserModelDynamo, EnrollModelDynamo])
+const TableDynamo = new Table(process.env.TABLE_NAME, [UserModelDynamo])
 
 export const handler = serverless(app)
