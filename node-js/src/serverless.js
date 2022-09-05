d1317156dbf1c9d41b108e7a7f17dc8bcfb35bd3
Giovanna G B Fritsche <ggarciabas@gmail.com>
2022-09-04 21:34:30 -0300

    fix: import table

--- node-js/src/serverless.js
+++ node-js/src/serverless.js
@@ -1,8 +1,9 @@
 import app from "./api/app.js";
 import serverless from 'serverless-http';
+import dynamoose from "dynamoose";
 import {UserModelDynamo} from './model/user-model.js';
 import {EnrollModelDynamo} from './model/enroll-model.js';
 
-const TableDynamo = new Table(process.env.TABLE_NAME, [UserModelDynamo, EnrollModelDynamo])
+const TableDynamo = new dynamoose.Table(process.env.TABLE_NAME, [UserModelDynamo, EnrollModelDynamo])
 
 export const handler = serverless(app)
