bfaca4a94f020c3b9ae113dbca358c370120ab5a
Giovanna G B Fritsche <ggarciabas@gmail.com>
2023-02-14 14:47:54 -0300

    fix: route login

--- node-js/src/routers/login-routes.js
+++ node-js/src/routers/login-routes.js
@@ -4,6 +4,6 @@ import LoginController from '../controllers/login-controller.js';
 
 const loginRoutes = express.Router();
 
-loginRoutes.post('/login', rescue(LoginController.doLogin));
+loginRoutes.post('/', rescue(LoginController.doLogin));
 
 export { loginRoutes }
