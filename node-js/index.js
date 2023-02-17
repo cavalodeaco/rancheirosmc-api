df7baf5187a8578818b5911653571815eb187220
Giovanna G B Fritsche <ggarciabas@gmail.com>
2023-02-17 11:21:30 -0300

    feat: update samcli and table creation relates to #48

--- node-js/index.js
+++ node-js/index.js
@@ -6,30 +6,33 @@ import app from "./src/api/app.js";
 // Create table if not exists
 import { dynamoDbClient } from './src/libs/ddb-client.js';
 import { CreateTableCommand } from "@aws-sdk/client-dynamodb";
-const params = {
-    TableName: `${process.env.TABLE_NAME}`,
-    KeySchema: [
-        { AttributeName: "PK", KeyType: "HASH" },
-        { AttributeName: "SK", KeyType: "RANGE" }
-    ],
-    AttributeDefinitions: [
-        { AttributeName: "PK", AttributeType: "S" },
-        { AttributeName: "SK", AttributeType: "S" }
-    ],
-    ProvisionedThroughput: {
-        ReadCapacityUnits: 1,
-        WriteCapacityUnits: 1
-    }
-};
-dynamoDbClient.send(new CreateTableCommand(params)).then((data) => {
-    console.log("Table Created", data);
-}).catch((err) => {
-    if (err.name === "ResourceInUseException") {
-        console.log("Table already exists");
-    } else {
-        throw err;
-    }
-});
+
+for (const table of ["user", "enroll"]) {
+    const params = {
+        TableName: `${process.env.TABLE_NAME}-${table}`,
+        KeySchema: [
+            { AttributeName: "PK", KeyType: "HASH" },
+            { AttributeName: "SK", KeyType: "RANGE" }
+        ],
+        AttributeDefinitions: [
+            { AttributeName: "PK", AttributeType: "S" },
+            { AttributeName: "SK", AttributeType: "S" }
+        ],
+        ProvisionedThroughput: {
+            ReadCapacityUnits: 1,
+            WriteCapacityUnits: 1
+        }
+    };
+    dynamoDbClient.send(new CreateTableCommand(params)).then((data) => {
+        console.log(`Table ${table} Created `, data);
+    }).catch((err) => {
+        if (err.name === "ResourceInUseException") {
+            console.log(`Table ${table} already exists `);
+        } else {
+            throw err;
+        }
+    });
+}
 
 // import dynamoose from "dynamoose";
 // import { UserModelDynamo } from './src/model/user-model.js';
