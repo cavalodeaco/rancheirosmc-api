aea9c2a710484427709b4113c744188c51dadbb9
Giovanna G B Fritsche <ggarciabas@gmail.com>
2023-03-06 14:26:42 -0300

    feat: update middleware

--- node-js/src/middleware/cors-middleware.js
+++ node-js/src/middleware/cors-middleware.js
@@ -1,10 +1,10 @@
 
 export default (err, req, res, next) => {
     console.log("Cors Middleware");
-    if (process.env.NODE_ENV === 'production') {
+    if (process.env.ENV === 'production') {
         res.header('Access-Control-Allow-Origin', 'https://ppv.lordriders.com, https://ppv-admin.lordriders.com');
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, id_token, access_token');
-        res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,limit,page,access_token,id_token');
+        res.header('Access-Control-Allow-Methods', 'GET,POST,HEAD,OPTIONS');
         res.header('Access-Control-Allow-Credentials', 'true');
         next();
     } else {
