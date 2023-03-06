ac7f0f7c60fbb1cce8d04118aa86958899dd7fb5
Giovanna G B Fritsche <ggarciabas@gmail.com>
2023-03-06 14:40:32 -0300

    feat: cors

--- node-js/src/middleware/cors-middleware.js
+++ node-js/src/middleware/cors-middleware.js
@@ -2,16 +2,16 @@
 export default (err, req, res, next) => {
     console.log("Cors Middleware");
     if (process.env.ENV === 'production') {
-        res.header('Access-Control-Allow-Origin', 'https://ppv.lordriders.com, https://ppv-admin.lordriders.com');
-        res.header('Access-Control-Allow-Headers', 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,limit,page,access_token,id_token');
-        res.header('Access-Control-Allow-Methods', 'GET,POST,HEAD,OPTIONS');
-        res.header('Access-Control-Allow-Credentials', 'true');
+        res.header('Access-Control-Allow-Origin', 'https://lordriders.com');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization,limit,page,access_token,id_token');
+        res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+        res.header('Access-Control-Allow-Credentials', 'false');
         next();
     } else {
         res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, id_token, access_token');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization, limit,page,access_token,id_token');
         res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-        res.header('Access-Control-Allow-Credentials', 'true');
+        res.header('Access-Control-Allow-Credentials', 'false');
         next();
     }
 }
