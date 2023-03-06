9f6a7e153bfc67652cbddd44eed910a1e322f36d
Giovanna G B Fritsche <ggarciabas@gmail.com>
2023-03-06 13:36:00 -0300

    feat: cors only on production

--- node-js/src/api/app.js
+++ node-js/src/api/app.js
@@ -1,24 +1,26 @@
 import cors from 'cors';
 import express from 'express';
 import bodyParser from 'body-parser';
-import {loginRoutes} from '../routers/login-routes.js';
-import {enrollRoutes} from '../routers/enroll-routes.js';
+import { loginRoutes } from '../routers/login-routes.js';
+import { enrollRoutes } from '../routers/enroll-routes.js';
 import errorMiddleware from '../middleware/error-middleware.js';
 import { reportRoutes } from '../routers/report-routes.js';
 
 const app = express()
 
-var allowlist = ['https://ppv.lordriders.com, https://ppv-admin.lordriders.com']
-var corsOptionsDelegate = function (req, callback) {
-  var corsOptions;
-  if (allowlist.indexOf(req.header('Origin')) !== -1) {
-    console.log("Allowing CORS for: " + req.header('Origin'));
-    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-  } else {
-    console.log("Disabling CORS for: " + req.header('Origin'));
-    corsOptions = { origin: false } // disable CORS for this request
-  }
-  callback(null, corsOptions) // callback expects two parameters: error and options
+if (process.env.NODE_ENV === 'production') {
+    var allowlist = ['https://ppv.lordriders.com, https://ppv-admin.lordriders.com']
+    var corsOptionsDelegate = function (req, callback) {
+        var corsOptions;
+        if (allowlist.indexOf(req.header('Origin')) !== -1) {
+            console.log("Allowing CORS for: " + req.header('Origin'));
+            corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+        } else {
+            console.log("Disabling CORS for: " + req.header('Origin'));
+            corsOptions = { origin: false } // disable CORS for this request
+        }
+        callback(null, corsOptions) // callback expects two parameters: error and options
+    }
 }
 
 app.use(bodyParser.json())
