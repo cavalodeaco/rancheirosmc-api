eafbc9f480578109017aa89242a5d976982f2acf
Giovanna G B Fritsche <ggarciabas@gmail.com>
2023-03-06 22:29:34 -0300

    feat: log midd

--- node-js/src/routers/login-routes.js
+++ node-js/src/routers/login-routes.js
@@ -2,9 +2,10 @@ import express from 'express';
 import rescue from 'express-rescue';
 import LoginController from '../controllers/login-controller.js';
 import corsMiddleware from '../middleware/cors-middleware.js';
+import logMiddleware from '../middleware/log-middleware.js';
 
 const loginRoutes = express.Router();
 
-loginRoutes.post('/', rescue(LoginController.doLogin), rescue(corsMiddleware));
+loginRoutes.post('/', rescue(logMiddleware), rescue(LoginController.doLogin), rescue(corsMiddleware));
 
 export { loginRoutes }
--- node-js/src/routers/report-routes.js
+++ node-js/src/routers/report-routes.js
@@ -2,17 +2,18 @@ import express from 'express';
 import rescue from 'express-rescue';
 import ReportController from '../controllers/report-controller.js';
 import JWTMiddleware from '../middleware/jwt-middleware.js';
+import logMiddleware from '../middleware/log-middleware.js';
 import corsMiddleware from '../middleware/cors-middleware.js';
 
 const reportRoutes = express.Router();
 const jwtMiddleware = new JWTMiddleware();
 const repControl = new ReportController();
 
-reportRoutes.get('/enroll', rescue(jwtMiddleware.validateToken), (repControl.getEnrolls), rescue(corsMiddleware));
-reportRoutes.get('/enroll/city/:city', rescue(jwtMiddleware.validateToken), rescue(repControl.getEnrollsByCity), rescue(corsMiddleware));
-reportRoutes.get('/enroll/status/:status', rescue(jwtMiddleware.validateToken), rescue(repControl.getEnrollsByStatus), rescue(corsMiddleware));
-reportRoutes.get('/enroll/id/:id', rescue(jwtMiddleware.validateToken), rescue(repControl.getEnrollById), rescue(corsMiddleware));
-reportRoutes.get('/user', rescue(jwtMiddleware.validateToken), rescue(repControl.getUsers), rescue(corsMiddleware));
-reportRoutes.get('/user/id/:id', rescue(jwtMiddleware.validateToken), rescue(repControl.getUserById), rescue(corsMiddleware));
+reportRoutes.get('/enroll', rescue(logMiddleware), rescue(jwtMiddleware.validateToken), (repControl.getEnrolls), rescue(corsMiddleware));
+reportRoutes.get('/enroll/city/:city', rescue(logMiddleware), rescue(jwtMiddleware.validateToken), rescue(repControl.getEnrollsByCity), rescue(corsMiddleware));
+reportRoutes.get('/enroll/status/:status', rescue(logMiddleware), rescue(jwtMiddleware.validateToken), rescue(repControl.getEnrollsByStatus), rescue(corsMiddleware));
+reportRoutes.get('/enroll/id/:id', rescue(logMiddleware), rescue(jwtMiddleware.validateToken), rescue(repControl.getEnrollById), rescue(corsMiddleware));
+reportRoutes.get('/user', rescue(logMiddleware), rescue(jwtMiddleware.validateToken), rescue(repControl.getUsers), rescue(corsMiddleware));
+reportRoutes.get('/user/id/:id', rescue(logMiddleware), rescue(jwtMiddleware.validateToken), rescue(repControl.getUserById), rescue(corsMiddleware));
 
 export { reportRoutes };
