c3bab5619fe0b6243b39c08673c7b0ef3c137d23
Giovanna G B Fritsche <ggarciabas@gmail.com>
2023-03-10 08:03:22 -0300

    fix: import

--- node-js/src/routers/class-routes.js
+++ node-js/src/routers/class-routes.js
@@ -3,8 +3,10 @@ import rescue from 'express-rescue';
 import corsMiddleware from '../middleware/cors-middleware.js';
 import logMiddleware from '../middleware/log-middleware.js';
 import ClassController from '../controllers/class-controller.js';
+import JWTMiddleware from '../middleware/jwt-middleware.js';
 
 const classRoutes = express.Router();
+const jwtMiddleware = new JWTMiddleware();
 
 classRoutes.post('/', rescue(logMiddleware), rescue(jwtMiddleware.validateToken), rescue(ClassController.post), rescue(corsMiddleware));
 classRoutes.get('/', rescue(logMiddleware), rescue(jwtMiddleware.validateToken), rescue(ClassController.get), rescue(corsMiddleware));
