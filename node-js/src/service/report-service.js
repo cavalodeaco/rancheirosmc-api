312d4e48e563d966336fbc2242922959023eaaab
Giovanna G B Fritsche <ggarciabas@gmail.com>
2023-03-21 20:49:51 -0300

    fix: header

--- node-js/src/service/report-service.js
+++ node-js/src/service/report-service.js
@@ -1,43 +1,22 @@
 import {EnrollModelDb as EnrollModel} from "../model/enroll-model-db.js";
 import { UserModelDb as UserModel } from "../model/user-model-db.js";
 import Ajv from 'ajv';
-
-const GetEnrollSchema = {
-    type: "object",
-    properties: {
-        filter: { type: "string" }
-    },
-    required: ["filter"],
-    additionalProperties: false
-}
+import CreateError from 'http-errors';
 
 class ReportService {
     async getEnrollById (id) {
         console.log("Service: getEnrollById");
         return { status: 200, data: await EnrollModel.getById(id)}
     }
-    async getEnrolls (limit, page, data) {
+    async getEnrolls (limit, page, filter) {
         console.log("Service: getEnrolls");
-        this.validateGetEnrolls(data);
-        if (data.filter == "all")
+        if (filter == "all")
             return { status: 200, data: await EnrollModel.get(limit, page)}
-        else if (data.filter == "rancho")
+        else if (filter == "rancho")
             return { status: 200, data: await EnrollModel.getRancho(limit, page)}
-        else if (data.filter == "curitiba")
+        else if (filter == "curitiba")
             return { status: 200, data: await EnrollModel.getCuritiba(limit, page)}
-        throw CreateError[400]({ message: `Invalid filter: ${data.filter}` });        
-    }
-
-    validateGetEnrolls(data) {
-        // Validade main structure
-        const ajv = new Ajv({ allErrors: true })
-        const valid = ajv.validate(GetEnrollSchema, data)
-        if (!valid) {
-            const mp = ajv.errors.map((error) => {
-                return error.params.missingProperty;
-            });
-            throw CreateError[400]({ message: `Missing property on body: ${mp}` });
-        }
+        throw CreateError[400]({ message: `Invalid filter ${filter}` });
     }
 
     async getEnrollsByCity(city, limit, page) {
