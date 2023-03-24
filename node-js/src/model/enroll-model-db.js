8f5fc79580ebd4b01d5aab67c7651f84b59f7d8d
Giovanna G B Fritsche <ggarciabas@gmail.com>
2023-03-23 22:02:47 -0300

    fix: ctb

--- node-js/src/model/enroll-model-db.js
+++ node-js/src/model/enroll-model-db.js
@@ -189,7 +189,10 @@ class EnrollModelDb {
             TableName: `${process.env.TABLE_NAME}-enroll`,
             Limit: parseInt(limit),
             ExclusiveStartKey: page,
-            KeyConditionExpression: "city = curitiba",
+            KeyConditionExpression: "city = :city",
+            ExpressionAttributeValues: {
+                ":city": "curitiba"
+            }
         };
         if (page === undefined || page === 0) {
             delete params.ExclusiveStartKey;
