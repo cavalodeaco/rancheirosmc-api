dacf07be0e2bcbfaff07ad1e20f6e828c96f390c
Giovanna G B Fritsche <ggarciabas@gmail.com>
2023-03-25 15:21:33 -0300

    fix: validate token managers

--- node-js/src/routers/legacy-routes.js
+++ node-js/src/routers/legacy-routes.js
@@ -3,9 +3,11 @@ import express from 'express';
 import rescue from 'express-rescue';
 import corsMiddleware from '../middleware/cors-middleware.js';
 import logMiddleware from '../middleware/log-middleware.js';
+import JWTMiddleware from '../middleware/jwt-middleware.js';
 
 const legacyRoutes = express.Router();
+const jwtMiddleware = new JWTMiddleware();
 
-legacyRoutes.post('/', rescue(logMiddleware), rescue(LegacyController.postEnroll), rescue(corsMiddleware));
+legacyRoutes.post('/', rescue(logMiddleware), rescue(jwtMiddleware.validateToken), rescue(LegacyController.postEnroll), rescue(corsMiddleware));
 
 export { legacyRoutes };
--- node-js/src/routers/manager-routes.js
+++ node-js/src/routers/manager-routes.js
@@ -3,13 +3,15 @@ import express from 'express';
 import rescue from 'express-rescue';
 import corsMiddleware from '../middleware/cors-middleware.js';
 import logMiddleware from '../middleware/log-middleware.js';
+import JWTMiddleware from '../middleware/jwt-middleware.js';
 
 const managerRoutes = express.Router();
+const jwtMiddleware = new JWTMiddleware();
 
-managerRoutes.post('/call', rescue(logMiddleware), rescue(ManagerController.postCall), rescue(corsMiddleware));
-managerRoutes.post('/confirm', rescue(logMiddleware), rescue(ManagerController.postConfirm), rescue(corsMiddleware));
-managerRoutes.post('/certify', rescue(logMiddleware), rescue(ManagerController.postCertify), rescue(corsMiddleware));
-managerRoutes.post('/miss', rescue(logMiddleware), rescue(ManagerController.postMiss), rescue(corsMiddleware));
-managerRoutes.post('/drop', rescue(logMiddleware), rescue(ManagerController.postDrop), rescue(corsMiddleware));
+managerRoutes.post('/call', rescue(logMiddleware), rescue(jwtMiddleware.validateToken), rescue(ManagerController.postCall), rescue(corsMiddleware));
+managerRoutes.post('/confirm', rescue(logMiddleware), rescue(jwtMiddleware.validateToken), rescue(ManagerController.postConfirm), rescue(corsMiddleware));
+managerRoutes.post('/certify', rescue(logMiddleware), rescue(jwtMiddleware.validateToken), rescue(ManagerController.postCertify), rescue(corsMiddleware));
+managerRoutes.post('/miss', rescue(logMiddleware), rescue(jwtMiddleware.validateToken), rescue(ManagerController.postMiss), rescue(corsMiddleware));
+managerRoutes.post('/drop', rescue(logMiddleware), rescue(jwtMiddleware.validateToken), rescue(ManagerController.postDrop), rescue(corsMiddleware));
 
 export { managerRoutes };
