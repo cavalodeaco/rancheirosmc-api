d3215f39f7da51e60388fedbd425173683a88a1f
Gian Fritsche <gian.fritsche@betrybe.com>
2023-03-31 13:23:16 -0300

    separate mock jsonwebtoken, tokens and process.env related to login

--- node-js/test/jwt-middleware.test.js
+++ node-js/test/jwt-middleware.test.js
@@ -1,70 +1,17 @@
-const axios = require("axios");
-jest.mock("axios");
+const tokens = require('./mocks/login.mock');
 
-axios.get.mockResolvedValueOnce({
-  data: {
-    keys: []
-  }
-});
-
-let JWTMiddleware;
+const JWTMiddleware = require('../src/middleware/jwt-middleware');
 
 describe('The JWTMiddleware.validateToken method on production', function () {
 
-  const originalEnv = process.env;
-
-  const tokens = {
-    AccessToken: "access",
-    IdToken: "id",
-    RefreshToken: 'refresh',
+  process.env = {
+    ...process.env,
+    ENV: 'production',
   };
 
-  beforeEach(() => {
-    process.env = {
-      ...originalEnv,
-      ENV: 'production',
-      CLIENT_ID: 'client_id',
-      AWS_REGION: 'us-east-1',
-      USER_POOL_ID: 'POOL_ID',
-    };
-
-    jest.mock('jsonwebtoken', () => {
-      const originalModule = jest.requireActual('jsonwebtoken');
-      return ({
-        ...originalModule,
-        decode: (token) => ({
-          payload: {
-            exp: Infinity, // never expires
-            aud: process.env.CLIENT_ID,
-            client_id: process.env.CLIENT_ID,
-            iss: `https://cognito-idp.${process.env.AWS_REGION}.amazonaws.com/${process.env.USER_POOL_ID}`,
-            token_use: token,
-          },
-          header: {
-            kid: 'constructor', // or any other object prototype attribute making the empty object pems[kid] to be valid
-          }
-        }),
-        verify: () => { },
-      })
-    });
-
-    JWTMiddleware = require('../src/middleware/jwt-middleware');
-
-  });
-
-  afterEach(() => {
-    process.env = originalEnv;
-    jest.mock('jsonwebtoken', () => jest.requireActual('jsonwebtoken'));
-  });
-
   it('should run without exceptions and call next for a successful auth', async () => {
-    const req = {
-      headers: {
-        id_token: tokens.IdToken,
-        access_token: tokens.AccessToken,
-      },
-    };
-    const nextReturn = await new JWTMiddleware().validateToken(req, undefined, () => "SUCCESS");
+    const nextReturn = await new JWTMiddleware()
+      .validateToken({ headers: tokens }, undefined, () => "SUCCESS");
     expect(nextReturn).toBe('SUCCESS');
   });
 
---
+++ node-js/test/mocks/login.mock.js
@@ -0,0 +1,40 @@
+const axios = require("axios");
+jest.mock("axios");
+
+axios.get.mockResolvedValueOnce({
+  data: {
+    keys: []
+  }
+});
+
+const tokens = {
+  access_token: "access",
+  id_token: "id",
+  refresh_token: 'refresh',
+};
+
+process.env = {
+  ...process.env,
+  CLIENT_ID: 'client_id',
+  AWS_REGION: 'us-east-1',
+  USER_POOL_ID: 'POOL_ID',
+};
+
+jest.mock('jsonwebtoken', () => ({
+  ...jest.requireActual('jsonwebtoken'),
+  decode: (token) => ({
+    payload: {
+      exp: Infinity, // never expires
+      aud: process.env.CLIENT_ID,
+      client_id: process.env.CLIENT_ID,
+      iss: `https://cognito-idp.${process.env.AWS_REGION}.amazonaws.com/${process.env.USER_POOL_ID}`,
+      token_use: token,
+    },
+    header: {
+      kid: 'constructor', // or any other object prototype attribute making the empty object pems[kid] to be valid
+    },
+  }),
+  verify: () => { },
+}));
+
+module.exports = tokens;
