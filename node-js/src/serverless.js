1b2a6e20ff14eebe092881b507f46412be3283d1
Giovanna G B Fritsche <ggarciabas@gmail.com>
2023-04-03 19:32:46 -0300

    fix: lambda serverless handler

--- node-js/src/serverless.js
+++ node-js/src/serverless.js
@@ -1,6 +1,4 @@
 const app = require("./api/app.js");
 const serverless = require("serverless-http");
 
-const handler = serverless(app);
-
-module.exports = handler;
+exports.handler = serverless(app);
