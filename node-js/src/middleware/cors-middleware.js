8226fb76f9d0a6643e07d05d4ceeb4b776afcf7e
Giovanna G B Fritsche <ggarciabas@gmail.com>
2023-04-03 22:11:46 -0300

    fix: cors

--- node-js/src/middleware/cors-middleware.js
+++ node-js/src/middleware/cors-middleware.js
@@ -24,6 +24,6 @@ module.exports = (req, res, next) => {
     );
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
     res.header("Access-Control-Allow-Credentials", "false");
-    return next();
   }
+  return next();
 };
