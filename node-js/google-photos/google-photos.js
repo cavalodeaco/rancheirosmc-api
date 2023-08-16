3a7aca0c04e46cb55c6875de92f0350b0996727e
Giovanna G B Fritsche <ggarciabas@gmail.com>
2023-08-15 23:00:03 -0300

    fix: cors and get only 10 pics

--- node-js/google-photos/google-photos.js
+++ node-js/google-photos/google-photos.js
@@ -1,22 +1,25 @@
-const axios = require('axios');
+const axios = require("axios");
 
-const regex = /\["(https:\/\/lh3\.googleusercontent\.com\/pw\/[a-zA-Z0-9\-_]*)"/g
+const regex =
+  /\["(https:\/\/lh3\.googleusercontent\.com\/pw\/[a-zA-Z0-9\-_]*)"/g;
 
 function extractPhotos(content) {
- const links = new Set()
-  let match
-  while (match = regex.exec(content)) {
-    links.add(match[1])
-  } 
-  return Array.from(links)
+  const links = new Set();
+  let match;
+  let total = 0;
+
+  while ((match = regex.exec(content)) && total < 10) {
+    links.add(match[1]);
+    total += 1;
+  }
+  return Array.from(links);
 }
 
 async function getAlbum(id) {
-  const response = await axios.get(`https://photos.app.goo.gl/${id}`)
-  return extractPhotos(response.data)
+  const response = await axios.get(`https://photos.app.goo.gl/${id}`);
+  return extractPhotos(response.data);
 }
-
 
 module.exports = {
- getAlbum 
-}
+  getAlbum,
+};
